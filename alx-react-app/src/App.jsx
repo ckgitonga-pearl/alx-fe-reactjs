2db1188e3f2c8539f5eaea935ca0d3c72a969c92
CK Gitonga <ckgitonga222@gmail.com>
2026-01-06 13:55:57 +0300

    Task 1: Modify JSX and render WelcomeMessage component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import WelcomeMessage from './components/WelcomeMessage';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -15,6 +16,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+       <WelcomeMessage />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
