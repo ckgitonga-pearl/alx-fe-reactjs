aab99973eb0f59e366e4042830cd21390b615eb9
CK Gitonga <ckgitonga222@gmail.com>
2026-01-06 14:14:33 +0300

    Task 2: Assemble Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,6 @@
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';import WelcomeMessage from './components/WelcomeMessage';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -17,6 +19,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
        <WelcomeMessage />
+       <Header />
+      <MainContent />
+      <Footer />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
