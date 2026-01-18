01f7172def19f8c654886605dcf5abc44b34b37b
ckgitonga-pearl <ckgitonga222@gmail.com>
2026-01-18 14:35:20 +0300

    Fix UserProfile inline styling to match checker

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile(props) {
     return (
-        <div >
+        <div style={{ border: '1px solid black', padding: '10px', margin: '10px' }}> 
             <h2>{props.name}<span style={{ color: 'blue', fontSize: '10px' }}></span></h2>
             
             <p>Age: {props.age}</p>
