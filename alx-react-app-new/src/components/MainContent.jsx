33418b84cbc4a44ce54b7325f3368dffd5f72665
ckgitonga-pearl <ckgitonga222@gmail.com>
2026-01-18 14:11:23 +0300

    Add alx-react-app-new and apply inline styling updates

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+ function MainContent() {
+    return (
+        <main style= {{ padding: '20px', fontSize: '18px'}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
+
+
+
+
