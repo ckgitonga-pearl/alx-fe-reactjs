f2532759ca0bae156b0bec076cc14aa922e50c2f
ckgitonga-pearl <ckgitonga222@gmail.com>
2026-01-18 15:35:37 +0300

    Add inline flex styling to Navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#333' }}>
+    <nav style={{ display: 'flex', justifyContent: 'space-between', padding: '10px', backgroundColor: '#333' }}>
       <Link to="/" style={{ color: 'white', marginRight: '10px' }}>Home</Link>
       <Link to="/about" style={{ color: 'white', marginRight: '10px' }}>About</Link>
       <Link to="/services" style={{ color: 'white', marginRight: '10px' }}>Services</Link>
