c2cf97a8f242bbf5857be9500f04c74f78f1dd8d
ckgitonga-pearl <ckgitonga222@gmail.com>
2026-01-25 16:45:04 +0300

    update all the files

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,32 @@
-import AddRecipeForm from './components/AddRecipeForm';
-import RecipeList from './components/RecipeList';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RoutesComponent from "./RoutesComponent";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <BrowserRouter>
+    <RoutesComponent />
+    </BrowserRouter>
+     );
+      <div style={{ padding: "2rem" }}>
+        <h1>Recipe Sharing App</h1>
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipes/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -1,43 +1,40 @@
 import { useState } from "react";
 import { useRecipeStore } from "./recipeStore";
 
-const AddRecipeForm = () => {
-  const addRecipe = useRecipeStore((state) => state.addRecipe);
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
+const EditRecipeForm = ({ recipe }) => {
+  const updateRecipe = useRecipeStore((state) => state.updateRecipe);
+
+  const [title, setTitle] = useState(recipe.title);
+  const [description, setDescription] = useState(recipe.description);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (!title.trim() || !description.trim()) return;
 
-    addRecipe({
-      id: Date.now(),
+    updateRecipe({
+      id: recipe.id,
       title,
       description,
     });
-
-    setTitle("");
-    setDescription("");
   };
 
   return (
     <form onSubmit={handleSubmit}>
+      <h3>Edit Recipe</h3>
+
       <input
         type="text"
-        placeholder="Title"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
       />
 
       <textarea
-        placeholder="Description"
         value={description}
         onChange={(e) => setDescription(e.target.value)}
       />
 
-      <button type="submit">Add Recipe</button>
+      <button type="submit">Update Recipe</button>
     </form>
   );
 };
 
-export default AddRecipeForm;
+export default EditRecipeForm;
