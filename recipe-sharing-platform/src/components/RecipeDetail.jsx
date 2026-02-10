8c7f542b56f73c186a5739ca0b5a497dc9456c96
ckgitonga-pearl <ckgitonga222@gmail.com>
2026-02-10 16:39:00 +0300

    shadow effect added on recipeDetail.jsx

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -18,7 +18,7 @@ const RecipeDetail = () => {
   }
 
   return (
-    <div className="max-w-4xl mx-auto p-6">
+    <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-lg">
       <img
         src={recipe.image}
         alt={recipe.title}
