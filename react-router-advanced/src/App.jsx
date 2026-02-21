41d4842750ae0235667f8de1829bb1dc7d247482
ckgitonga-pearl <ckgitonga222@gmail.com>
2026-02-21 12:11:19 +0300

    Fix: remove submodule and add react-router-advanced as normal folder

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,33 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import ProfileDetails from "./pages/ProfileDetails";
+import ProfileSettings from "./pages/ProfileSettings";
+import BlogPost from "./pages/BlogPost";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function App() {
+  return (
+    <Routes>
+
+      <Route path="/" element={<Home />} />
+
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      >
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Route>
+
+      <Route path="/blog/:id" element={<BlogPost />} />
+
+    </Routes>
+  );
+}
+
+export default App;
---
+++ react-router-advanced/src/pages/Profile.jsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router-dom";
+
+function Profile() {
+  return (
+    <div>
+      <h2>Profile</h2>
+      <Outlet />
+    </div>
+  );
+}
